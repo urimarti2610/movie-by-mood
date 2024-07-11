da37b683c69b7369745a1c1e330ddcede990929f
Oriol Martí <omarti@theknotww.com>
2024-07-11 16:56:07 +0200

    Last changes

--- src/libs/themoviedb/client.js
+++ src/libs/themoviedb/client.js
@@ -1,10 +1,10 @@
 export class TheMovieDbClient {
     locale = Intl.DateTimeFormat().resolvedOptions().locale
-    apiKey = 'cea68b520beecac6718820e4ac576c3a'
+    apiKey = import.meta.env.THE_MOVIE_DB_API_KEY
     baseUrl = 'https://api.themoviedb.org/3/'
 
     async getMoviesByGenreId(id) {
-        const response = await fetch(`${this.baseUrl}discover/movie?include_adult=true&include_video=true&with_genres=${id}&api_key=${this.apiKey}&language=${this.locale}`)
+        const response = await fetch(`${this.baseUrl}discover/movie?include_adult=true&include_video=true&with_genres=${id}&api_key=${this.apiKey}`)
         const { results } = await response.json()
 
         return results
@@ -15,6 +15,8 @@ export class TheMovieDbClient {
 
         const response = await fetch(`${this.baseUrl}movie/${movie.id}/watch/providers?api_key=${this.apiKey}`)
         const { results } = await response.json()
+
+        if (!results) return {}
 
         return results[locale] ?? {}
     }
